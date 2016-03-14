cbb6a27833796a45ae111722b8a63593bd3752ea
Peter Jin <jinqiu.peter@gmail.com>
2016-03-14 09:59:36 +0800

    first commit

---
+++ apps/allin/WebSocketInstance.lua
@@ -0,0 +1,147 @@
+local Session = cc.import("#session")
+local Online = cc.import("#online")
+
+local gbc = cc.import("#gbc")
+local WebSocketInstance = cc.class("WebSocketInstance", gbc.WebSocketInstanceBase)
+
+local _EVENT = table.readonly({
+    ALLIN_MESSAGE       = "ALLIN_MESSAGE",
+    DISCONNECT          = "DISCONNECT"
+})
+WebSocketInstance.EVENT = _EVENT
+
+function WebSocketInstance:ctor(config)
+    WebSocketInstance.super.ctor(self, config)
+    self:addEventListener(WebSocketInstance.super.EVENT.CONNECTED, cc.handler(self, self.onConnected))
+    self:addEventListener(WebSocketInstance.super.EVENT.DISCONNECTED, cc.handler(self, self.onDisconnected))
+end
+
+function WebSocketInstance:onConnected()
+    -- do nothing
+    self:sendMessage({message = "Welcome back, " .. self._nickname})
+    -- load session
+    local redis = self:getRedis();
+    local session = Session:new(redis)
+
+    session:start()
+    local username = self:getPhone()
+    session:set("username", username)
+    session:save()
+
+    -- add user to online users list
+    local online = Online:new(self)
+    online:add(username, self:getConnectId())
+
+    self._session = session
+    self._online = online
+end
+
+function WebSocketInstance:onDisconnected(event)
+    if event.reason ~= gbc.Constants.CLOSE_CONNECT then
+        -- connection interrupted unexpectedly, remove user from online list
+        cc.printwarn("[websocket:%s] connection interrupted unexpectedly", self:getConnectId())
+        local username = self._session:get("username")
+        self._online:remove(username)
+    end
+
+    self:dispatchEvent({
+        name    = _EVENT.DISCONNECT,
+        websocket = self,
+        mysql     = mysql
+    })
+end
+
+function WebSocketInstance:validateSession(session)
+    local mysql = self:getMysql()
+
+    local sql = "select * from user where session = \'".. session .. "\';"
+    local dbres, err, errno, sqlstate = mysql:query(sql)
+    if not dbres then
+        return nil
+    end
+    if next(dbres) == nil then
+        return nil
+    end
+
+    self._nickname = dbres[1].nickname
+    self._cid = dbres[1].id
+    self._phone = dbres[1].phone
+    self._allinSession = session
+    self._installation = dbres[1].installation 
+    return true
+end
+
+function WebSocketInstance:getAllinSession()
+    return self._allinSession
+end
+
+function WebSocketInstance:getNickname()
+    return self._nickname
+end
+
+function WebSocketInstance:getCid()
+    return self._cid
+end
+
+function WebSocketInstance:getPhone()
+    return self._phone
+end
+
+function WebSocketInstance:getInstallation()
+    return self._installation
+end
+
+function WebSocketInstance:getClubIds()
+    local mysql = self:getMysql()
+    local sql = "SELECT club_id from user_club WHERE deleted = 0 AND user_id = ".. self:getCid() .. ";"
+    local dbres, err, errno, sqlstate = mysql:query(sql)
+    if not dbres then
+        return nil
+    end
+    if next(dbres) == nil then
+        return nil
+    end
+    
+    local clubs = {}
+    while next(dbres) ~= nil do
+        table.merge(clubs, dbres[1])
+        table.remove(dbres, 1)
+    end
+
+    return clubs;
+end
+
+function WebSocketInstance:addCustomLoop()
+    -- in the thread handling message from allin server, mysql connection cannot be reused, otherwise a "context 2" error occurs
+    -- we need to create another mysql connection. TODO: find out why
+    local mysql = self:createMysql()
+    if not mysql then
+        cc.throw("cannot create mysql connection")
+    end
+
+    -- create connection to allin server
+    local allin, err = self:getAllin():makeAllinLoop(connectId, mysql)
+    if not allin then
+        cc.throw("Error creating connnection to allin server: %s", err)
+    end
+
+    allin:start(function(message, mysql)
+        cc.printdebug("dispatching response %s", message)
+        self:dispatchEvent({
+            name    = _EVENT.ALLIN_MESSAGE .. "_" .. self:getConnectId(),
+            message = message,
+            websocket = self,
+            mysql     = mysql
+        })
+    end)
+    self._allinloop = allin
+end
+
+function WebSocketInstance:stopCustomLoop()
+    if not self._allinloop then
+        self._allinloop.stop()
+        self._allinloop = nil
+    end
+end
+
+return WebSocketInstance
---
+++ apps/allin/actions/ClubAction.lua
@@ -0,0 +1,275 @@
+local string_split       = string.split
+local Constants = cc.import(".Constants", "..")
+local gbc = cc.import("#gbc")
+local ClubAction = cc.class("ClubAction", gbc.ActionBase)
+ClubAction.ACCEPTED_REQUEST_TYPE = "websocket"
+
+-- public methods
+function ClubAction:ctor(config)
+    ClubAction.super.ctor(self, config)
+end
+
+function ClubAction:createclubAction(args)
+    local data = args.data
+    local name = data.name
+    local result = {state_type = "action_state", data = {
+        action = args.action}
+    }
+
+    if not name then
+        cc.printinfo("argument not provided: \"name\"")
+        result.data.msg = "name not provided"
+        result.data.state = Constants.Error.ArgumentNotSet 
+        return result
+    end
+    local area = data.area
+    if not area then
+        cc.printinfo("argument not provided: \"area\"")
+        result.data.msg = "area not provided"
+        result.data.state = Constants.Error.ArgumentNotSet 
+        return result
+    end
+    local description = data.description
+    if not description then
+        local nickname = self:getInstance():getNickname()
+        description = nickname .. "'s club"
+        cc.printinfo("club description set to default: %s", description)
+    end
+
+
+    local instance = self:getInstance()
+    local mysql = instance:getMysql()
+
+    -- get next auto increment id
+    local club_id, err = instance:getNextId("club")
+    if not club_id then
+        result.data.state = Constants.Error.MysqlError
+        result.data.msg = "failed to get next_id for table club, err: %s" .. err
+        return result
+    end
+
+    -- insert into db
+    local sql = "INSERT INTO club (name, area, description, owner_id) "
+                      .. " VALUES (" .. instance:sqlQuote(name) .. ", "
+                               .. instance:sqlQuote(area) .. ", "
+                               ..  instance:sqlQuote(description) .. ", "
+                               .. "(SELECT id FROM user WHERE session = " .. instance:sqlQuote(self:getInstance():getAllinSession()) .. "));"
+    cc.printdebug("executing sql: %s", sql)
+    local dbres, err, errno, sqlstate = mysql:query(sql)
+    if not dbres then
+        result.data.state = Constants.Error.MysqlError
+        result.data.msg = "数据库错误: " .. err
+        return result
+    end
+
+    result.data.club_id = club_id
+    result.data.state = 0
+    result.data.msg = "club created"
+    return result
+end
+
+function ClubAction:clubinfoAction(args)
+    local data = args.data
+    local club_id = data.club_id
+    local result = {state_type = "action_state", data = {
+        action = args.action}
+    }
+
+    if not club_id then
+        result.data.msg = "club_id not provided"
+        result.data.state = Constants.Error.ArgumentNotSet
+        cc.printinfo("argument not provided: \"club_id\"")
+        return result
+    end
+
+    local instance = self:getInstance()
+    local mysql = instance:getMysql()
+    local sql = "SELECT id , name, owner_id, area, description" 
+                .. " FROM club WHERE id " .. club_id 
+    cc.printdebug("executing sql: %s", sql)
+    local dbres, err, errno, sqlstate = mysql:query(sql)
+    if not dbres then
+        result.data.state = Constants.Error.MysqlError
+        result.data.msg = "数据库错误: " .. err
+        return result
+    end
+    if next(dbres) == nil then
+        result.data.state = Constants.Error.NotExist
+        result.data.msg = "club not found: " .. club_id
+        return result
+    end
+
+    table.merge(result.data, dbres)
+    result.data.state = 0
+    result.data.msg = "club found"
+    return result
+end
+
+function ClubAction:listjoinedclubAction(args)
+    local data = args.data
+    local result = {state_type = "action_state", data = {
+        action = args.action}
+    }
+
+    local instance = self:getInstance()
+    local mysql = instance:getMysql()
+    local sql = "SELECT a.id, a.name, a.owner_id, a.description FROM club a, user_club b "
+                .. " WHERE b.deleted = 0 AND "
+                .. " a.id = b.club_id AND "
+                .. " b.user_id = " .. instance:getCid()
+    cc.printdebug("executing sql: %s", sql)
+    local dbres, err, errno, sqlstate = mysql:query(sql)
+    if not dbres then
+        result.data.state = Constants.Error.MysqlError
+        result.data.msg = "数据库错误: " .. err
+        return result
+    end
+
+    result.data.club_joined = dbres
+    result.data.state = 0
+    result.data.msg = #dbres .. " club(s) joined"
+    return result
+end
+
+function ClubAction:leaveclubAction(args)
+    local data = args.data
+    local club_id = data.club_id
+
+    if not club_id then
+        result.data.msg = "club_id not provided"
+        result.data.state = Constants.Error.ArgumentNotSet
+        cc.printinfo("argument not provided: \"club_id\"")
+        return result
+    end
+
+    local result = {state_type = "action_state", data = {
+        action = args.action}
+    }
+
+    local instance = self:getInstance()
+    local joined_clubs = instance:getClubIds()
+    if not table.contains(joined_clubs, club_id) then
+        result.data.msg = "you must be a member of the club to leave: " .. club_id
+        result.data.state = Constants.Error.LogicError
+        return result
+    end
+
+    local mysql = instance:getMysql()
+    local sql = "UPDATE user_club SET deleted = 1 "
+                .. " WHERE deleted = 0 "
+                .. " AND club_id = " .. club_id
+                .. " AND user_id = " .. instance:getCid()
+    cc.printdebug("executing sql: %s", sql)
+    local dbres, err, errno, sqlstate = mysql:query(sql)
+    if not dbres then
+        result.data.state = Constants.Error.MysqlError
+        result.data.msg = "数据库错误: " .. err
+        return result
+    end
+
+    result.data.state = 0
+    result.data.msg = "club left"
+    return result
+end
+
+function ClubAction:listclubAction(args)
+    local data = args.data
+    local keyword = data.keyword
+    local limit = data.limit or Constants.Limit.ListClubLimit
+    local offset = data.offset or 0
+    local result = {state_type = "action_state", data = {
+        action = args.action}
+    }
+
+    if limit > 50 then
+        result.data.msg = "max number of record limit exceeded, only " .. Constants.Limit.ListClubLimit .. " allowed in one query"
+        result.data.state = PermissionDenied
+        return result
+    end
+
+    if not keyword then
+        result.data.msg = "keyword not provided"
+        result.data.state = Constants.Error.ArgumentNotSet
+        cc.printinfo("argument not provided: \"keyword\"")
+        return result
+    end
+
+    local instance = self:getInstance()
+    local mysql = instance:getMysql()
+    local word = '%' .. keyword .. '%'
+    local sql = "SELECT id, name, owner_id, area, description" 
+                .. " FROM club WHERE name like " .. instance:sqlQuote(word) 
+                .. " or area like " .. instance:sqlQuote(word) 
+                .. " or description like " .. instance:sqlQuote(word) 
+                .. " LIMIT " .. offset .. ", " .. limit
+    cc.printdebug("executing sql: %s", sql)
+    local dbres, err, errno, sqlstate = mysql:query(sql)
+    if not dbres then
+        result.data.state = Constants.Error.MysqlError
+        result.data.msg = "数据库错误: " .. err
+        return result
+    end
+
+    result.data.clubs = dbres
+    result.data.state = 0
+    result.data.msg = #dbres .. " club(s) found"
+    return result
+end
+
+function ClubAction:joinclubAction(args)
+    local data = args.data
+    local club_id = data.club_id
+    local text = data.text
+    local result = {state_type = "action_state", data = {
+        action = args.action}
+    }
+
+    if not club_id then
+        result.data.msg = "club_id not provided"
+        result.data.state = Constants.Error.ArgumentNotSet
+        cc.printinfo("argument not provided: \"club_id\"")
+        return result
+    end
+    
+    local instance = self:getInstance()
+    --TODO: add the application into a table and send to club owner for approval
+    local mysql = instance:getMysql()
+    local sql = "SELECT id FROM club WHERE id =" .. club_id 
+    cc.printdebug("executing sql: %s", sql)
+    local dbres, err, errno, sqlstate = mysql:query(sql)
+    if not dbres then
+        result.data.state = Constants.Error.MysqlError
+        result.data.msg = "数据库错误: " .. err
+        return result
+    end
+     
+    if #dbres == 0 then
+        result.data.state = Constants.Error.NotExist
+        result.data.msg = "club with club_id ".. club_id .. " not found"
+        return result
+    end
+
+    local joined_clubs = instance:getClubIds()
+    if table.contains(joined_clubs, club_id) then
+        result.data.msg = "you have already joined this club: " .. club_id
+        result.data.state = Constants.Error.LogicError
+        return result
+    end
+
+    sql = "INSERT INTO user_club (user_id, club_id) "
+                      .. " VALUES (" .. instance:getCid() .. ", " .. club_id .. ") "
+                      .. " ON DUPLICATE KEY UPDATE deleted = 0"
+    cc.printdebug("executing sql: %s", sql)
+    local dbres, err, errno, sqlstate = mysql:query(sql)
+    if not dbres then
+        result.data.state = Constants.Error.MysqlError
+        result.data.msg = "数据库错误: " .. err
+        return result
+    end
+
+    result.data.state = 0
+    result.data.msg = "club joined in"
+    return result
+end
+
+return ClubAction
